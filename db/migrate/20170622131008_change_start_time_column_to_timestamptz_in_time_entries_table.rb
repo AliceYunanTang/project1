4da389dc4840a39430a326cc25ae118d0cef2104
AliceYunanTang <tangyunan@hotmail.com>
2017-06-22 23:37:23 +1000

    timezone

--- db/migrate/20170622131008_change_start_time_column_to_timestamptz_in_time_entries_table.rb
+++ db/migrate/20170622131008_change_start_time_column_to_timestamptz_in_time_entries_table.rb
@@ -1,6 +1,8 @@
 class ChangeStartTimeColumnToTimestamptzInTimeEntriesTable < ActiveRecord::Migration[5.0]
   def change
-    change_column :time_entries, :start_time,  :timestamptz
-    change_column :time_entries, :end_time,  :timestamptz
+    remove_column :time_entries, :start_time,  :datetime
+    add_column :time_entries, :start_time,  :timestamptz
+    remove_column :time_entries, :end_time,  :datetime
+    add_column :time_entries, :end_time,  :timestamptz
   end
 end
